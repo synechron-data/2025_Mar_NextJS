faaa6ce0304af61d2b3c3787b392fc78a1b7956e
manish.sharma <manishvbn@gmail.com>
2025-03-30 11:27:58 +0530

    Reducer Hook

--- Day2/next-core-app/src/app/(page-components)/counter/page.tsx
+++ Day2/next-core-app/src/app/(page-components)/counter/page.tsx
@@ -2,7 +2,8 @@ import { Metadata } from "next";
 import styles from './conference.module.css';
 // import CounterAssignment from "@/app/_components/counter/counter-assignment";
 // import CounterAssignment from "@/app/_components/counter/counter-assignment-fn";
-import CounterAssignment from "@/app/_components/counter/counter-assignment-fn-o";
+// import CounterAssignment from "@/app/_components/counter/counter-assignment-fn-o";
+import CounterAssignment from "@/app/_components/counter/counter-assignment-reducer";
 
 export const metadata:Metadata = {
   title: "Counter Page",
